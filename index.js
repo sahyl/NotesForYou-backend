64f57620f5b64fd91ae892541087ecfbbbac01b5
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2024-02-09 22:50:32 +0530

    backend1

---
+++ index.js
@@ -0,0 +1,18 @@
+const connectToMongo =require('./db');
+connectToMongo();
+const express = require('express')
+const app = express()
+app.use(express.json())
+const port = 3000
+
+
+app.use('/api/auth',require('./routes/auth'))
+ app.use('/api/notes',require('./routes/notes'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
